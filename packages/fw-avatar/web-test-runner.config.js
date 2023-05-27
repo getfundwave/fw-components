44fddd01734578fd964398282587c50a4b6dce33
Balraj Gahlot <balrajgahlot20@gmail.com>
2023-05-27 15:06:58 +0530

    Added the libraries and packages required

---
+++ packages/fw-avatar/web-test-runner.config.js
@@ -0,0 +1,43 @@
+import { legacyPlugin } from "@web/dev-server-legacy";
+import { playwrightLauncher } from "@web/test-runner-playwright";
+
+const mode = process.env.MODE || "dev";
+if (!["dev", "prod"].includes(mode)) {
+  throw new Error(`MODE must be "dev" or "prod", was "${mode}"`);
+}
+
+const browsers = {
+  chromium: playwrightLauncher({ product: "chromium" }),
+  firefox: playwrightLauncher({ product: "firefox" }),
+  webkit: playwrightLauncher({ product: "webkit" }),
+};
+
+const noBrowser = (b) => {
+  throw new Error(`No browser configured named '${b}'; using defaults`);
+};
+let commandLineBrowsers;
+try {
+  commandLineBrowsers = process.env.BROWSERS?.split(",").map(
+    (b) => browsers[b] ?? noBrowser(b)
+  );
+} catch (e) {
+  console.warn(e);
+}
+
+const configObj = {
+  rootDir: ".",
+  files: ["./**/*test.js"],
+  nodeResolve: { exportConditions: mode === "dev" ? ["development"] : [] },
+  preserveSymlinks: true,
+  browsers: commandLineBrowsers ?? Object.values(browsers),
+  testFramework: {
+    // https://mochajs.org/api/mocha
+    config: {
+      ui: "bdd",
+      timeout: "2000",
+    },
+  },
+  plugins: [legacyPlugin()],
+};
+
+export default configObj;
