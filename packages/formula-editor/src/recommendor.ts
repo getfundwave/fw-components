ea5889663de6ace89e3009e713eb12772d23afae
Parasdeep <parasdeep@getfundwave.com>
2025-02-20 18:14:50 +0530

    update: return empty array in case of no recommendations + formatted @recommender.ts

--- packages/formula-editor/src/recommendor.ts
+++ packages/formula-editor/src/recommendor.ts
@@ -1,27 +1,20 @@
-import {matchSorter} from 'match-sorter'
+import { matchSorter } from "match-sorter";
 
 export class Recommender {
   private _minimumSuggestionLength: number;
-  private variableList: Array<string>;
+  private variableList: string[];
 
-  constructor(variables: Map<string, number>, minSuggestionLen: number) {
+  constructor(variables: string[], minSuggestionLen: number) {
     this._minimumSuggestionLength = minSuggestionLen > 0 ? minSuggestionLen : 1;
-    this.variableList = Array.from(variables.keys());
+    this.variableList = variables;
   }
 
-  getRecommendation(word: string): string[] | null {
-    if (word.length < this._minimumSuggestionLength) {
-      return null;
-    }
+  getRecommendation(word: string): string[] {
+    if (word.length < this._minimumSuggestionLength) return [];
 
-    const recommendations = matchSorter(this.variableList,word)
+    const recommendations = matchSorter(this.variableList, word);
 
-    if (
-      recommendations.length === 0 ||
-      (recommendations.length === 1 && recommendations[0] === word)
-    ) {
-      return null;
-    }
+    if (recommendations.length === 0 || (recommendations.length === 1 && recommendations[0] === word)) return [];
 
     return recommendations;
   }
