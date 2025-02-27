c1a4ba1781ea4f7947f5dcf42a65d1815d353aac
Govind-Jangra <govind70821@gmail.com>
2025-02-27 12:39:02 +0530

    chore: split tokens according to spaces if expty  send from parent component

--- packages/formula-editor/src/utils/get-formula-tokens.ts
+++ packages/formula-editor/src/utils/get-formula-tokens.ts
@@ -1,5 +1,7 @@
 export function getFormulaTokens(formulaString: string, formulaRegex: RegExp): string[] {
   if (!formulaString?.length) return [];
 
+  if(!Boolean(formulaRegex)) return formulaString.split(/(\s+)/) || [];
+
   return formulaString.match(formulaRegex) || [];
 }
